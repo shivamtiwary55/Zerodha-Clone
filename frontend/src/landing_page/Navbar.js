996ee33bd7b91aefa147077f090f051489235de9
shivamtiwary55 <shivamtiwary.aiml.aec@gmail.com>
2026-04-04 23:36:31 +0530

    fix logo path

--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../media/images/logo.svg"; // ✅ import karo
 
 function Navbar() {
   return (
@@ -10,52 +11,39 @@ function Navbar() {
       <div className="container p-2">
         <Link className="navbar-brand" to="/">
           <img
-            src="media/images/logo.svg"
+            src={logo}   // ✅ yaha use karo
             style={{ width: "25%" }}
             alt="Logo"
           />
         </Link>
+
         <button
           className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
         >
           <i className="fa fa-bars"></i>
         </button>
+
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <form className="d-flex" role="search">
-            <ul className="navbar-nav mb-lg-0">
-              <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/signup">
-                  Signup
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link active" to="/about">
-                  About
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link active" to="/product">
-                  Product
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link active" to="/pricing">
-                  Pricing
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link active" to="/support">
-                  Support
-                </Link>
-              </li>
-            </ul>
-          </form>
+          <ul className="navbar-nav mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link active" to="/signup">Signup</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link active" to="/about">About</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link active" to="/product">Product</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link active" to="/pricing">Pricing</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link active" to="/support">Support</Link>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
